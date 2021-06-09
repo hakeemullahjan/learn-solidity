ea0d8dc89d469527de1e9c1b14f884738b7369ed
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2021-06-09 21:13:48 +0500

    Calling Other Contracts

---
+++ CallingOtherContract.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.11;
+
+contract Callee{
+    uint public x;
+    uint public value;
+    
+     function setX(uint _x) public returns (uint){
+          x = _x;
+          return x;
+     }
+     
+      function setXAndSendEther(uint _x) public payable returns (uint, uint){
+          x =_x;
+          value = msg.value;
+          
+          return (x,value);
+      }
+}
+
+contract Caller{
+    function setX(Callee _callee,uint _x) public{
+        uint x = _callee.setX(_x);
+    } 
+    
+    function setXFromAddress(address _addr, uint _x) public {
+        Callee callee = Callee(_addr);
+        uint x = callee.setX(_x);
+    }
+    
+    function setXAndSendEther(Callee _callee, uint _x) public payable{
+        (uint x, uint value ) = _callee.setXAndSendEther.value(msg.value)(_x);
+    }  
+}
